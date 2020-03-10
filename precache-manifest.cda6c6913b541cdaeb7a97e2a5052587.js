97e6b9aaf8a2a8ce033da5936c8d8a1891f52ebe
unknown <deol.swati@gmail.com>
2020-03-10 23:05:36 +0530

    Updates

---
+++ precache-manifest.cda6c6913b541cdaeb7a97e2a5052587.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f15431fdb3ce4c424c44f405d8734ddc",
+    "url": "/react-app-pages/index.html"
+  },
+  {
+    "revision": "cac7fc9763ece6992598",
+    "url": "/react-app-pages/static/css/main.30beb401.chunk.css"
+  },
+  {
+    "revision": "135d73031af952a812a5",
+    "url": "/react-app-pages/static/js/2.e126e977.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-app-pages/static/js/2.e126e977.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cac7fc9763ece6992598",
+    "url": "/react-app-pages/static/js/main.1bcc26b5.chunk.js"
+  },
+  {
+    "revision": "20689a7367fe72853f5d",
+    "url": "/react-app-pages/static/js/runtime-main.cd270aa6.js"
+  }
+]);
